b47b0566bbda68c561660bb6d5039b8b00db0e44
Juca Crispim <juca@poraodojuca.dev>
2025-10-11 09:41:25 -0300

    Docs & bump version

--- docs/source/CHANGELOG.rst
+++ docs/source/CHANGELOG.rst
@@ -1,6 +1,10 @@
 Changelog
 =========
 
+* v0.10.3
+
+  - Update mongomotor
+
 * v0.10.2
 
   - Fix environment creation
--- toxicsecrets/__init__.py
+++ toxicsecrets/__init__.py
@@ -5,7 +5,7 @@
 from mongomotor import connect
 from toxiccore.conf import Settings
 
-__version__ = '0.10.2'
+__version__ = '0.10.3'
 
 settings = None
 dbconn = None
